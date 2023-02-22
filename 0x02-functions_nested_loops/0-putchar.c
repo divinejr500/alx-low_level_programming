5c780901c8c3b6a783360d2d4d8ae1748ad87e27
Odame Emmanuel <odameemmanuel14@gmail.com>
2023-02-21 20:20:06 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,14 @@
-/* This program prints _putcha, followed by a new line 
- *	this alway returns 0;
- * */
+#include "main.h"
+#include <unistd.h>
 
-#include <main.h>
-
-int main(void)
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-int _putchar(char c);
-
-return 0;
+	return (write(1, &c, 1));
 }
