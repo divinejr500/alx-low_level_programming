5aaef53029cc87435c0a1c8274a0a2bb69e1fe87
Odame Emmanuel <odameemmanuel14@gmail.com>
2023-02-28 08:09:27 -0800

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,11 +6,10 @@
 #include "main.h"
 
 /**
- * swap_int(int *a, int *b) - Takes a pointer to an int and updates
- *               the value it points to to 98.
+ * swap_int(int *a, int *b) - swap integers
  * @n: The pointer to an int.
  */
-void swap_int(int *a, int *b);
+void swap_int(int *a, int *b)
 {
 	int tmp = *a;
 	*a = *b;
